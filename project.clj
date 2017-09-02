6c2f1ce2ce3df124066af3453e20488d5f7c509e
Juan Vazquez <juanvazquez@gmail.com>
2017-09-02 16:51:03 -0500

    updating License

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
 (defproject enigma-machine "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Enigma machine written in clojure"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 ]
+  :license {:name "MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   :profiles {:dev {:dependencies [[midje "1.8.3"]]}})
